a2b46a7e03c046bf49f9f605ea03b73820802cd9
tylerpowers <tylerpowers4@gmail.com>
2025-02-11 11:41:25 -0500

    Initial Commit

---
+++ BoulderBud/BoulderBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderBudApp.swift
+//  BoulderBud
+//
+//  Created by Tyler Powers on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BoulderBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
